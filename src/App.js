9c62a9c786fa7c59781d97283ef5b2c032d792de
ArtemChubur <artemchubur@gmail.com>
2023-12-08 23:07:27 +0600

    pre-relise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 import Table from './components/table/table';
 
-function App() {
+function App() { 
   return (
     <div>
+        <Header />
         <Table />
     </div>
   );
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import './table.css'
 
 const Table = () => {
-    const[data, setData] = useState([])
+    const [data, setData] = useState([])
     const [cards, setCards] = useState([])
     const [cards2, setCards2] = useState([])
     const [enemyСards, setEnemyCards] = useState([
@@ -16,17 +17,27 @@ const Table = () => {
     const [cardValuePl1, setCardValuePl1] = useState(0)
     const [cardValuePl2, setCardValuePl2] = useState(0)
     const [cardsGivenAway, setCardsGivenAway] = useState(false)
+    const [hideEnemyCards, setHideEnemyCards] = useState(true)
 
     const [difference, setDifference] = useState(0)
     const [difference2, setDifference2] = useState(0)
 
+    const [winner, setWinner] = useState('')
+
+
 
     async function getDeck() {
         const response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle?count=1?shuffled=true')
-        setData(response.data)
+        try {
+            setData(response.data)
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async function drawCards() {
+        setHideEnemyCards(true)
+        setWinner('')
         const responsePl1 = await axios.get(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`)
         const responsePl2 = await axios.get(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`)
         try {
@@ -45,41 +56,41 @@ const Table = () => {
 
         if (scoreCard1Pl1 === "JACK") {
             scoreCard1Pl1 = '11'
-        } else if(scoreCard1Pl1 === "QUEEN") {
+        } else if (scoreCard1Pl1 === "QUEEN") {
             scoreCard1Pl1 = '12'
-        } else if(scoreCard1Pl1 === "KING") {
+        } else if (scoreCard1Pl1 === "KING") {
             scoreCard1Pl1 = '13'
-        } else if(scoreCard1Pl1 === "ACE") {
+        } else if (scoreCard1Pl1 === "ACE") {
             scoreCard1Pl1 = '14'
         }
 
         if (scoreCard2Pl1 === "JACK") {
             scoreCard2Pl1 = '11'
-        } else if(scoreCard2Pl1 === "QUEEN") {
+        } else if (scoreCard2Pl1 === "QUEEN") {
             scoreCard2Pl1 = '12'
-        } else if(scoreCard2Pl1 === "KING") {
+        } else if (scoreCard2Pl1 === "KING") {
             scoreCard2Pl1 = '13'
-        } else if(scoreCard2Pl1 === "ACE") {
+        } else if (scoreCard2Pl1 === "ACE") {
             scoreCard2Pl1 = '14'
         }
 
         if (scoreCard1Pl2 === "JACK") {
             scoreCard1Pl2 = '11'
-        } else if(scoreCard1Pl2 === "QUEEN") {
+        } else if (scoreCard1Pl2 === "QUEEN") {
             scoreCard1Pl2 = '12'
-        } else if(scoreCard1Pl2 === "KING") {
+        } else if (scoreCard1Pl2 === "KING") {
             scoreCard1Pl2 = '13'
-        } else if(scoreCard1Pl2 === "ACE") {
+        } else if (scoreCard1Pl2 === "ACE") {
             scoreCard1Pl2 = '14'
         }
 
         if (scoreCard2Pl2 === "JACK") {
             scoreCard2Pl2 = '11'
-        } else if(scoreCard2Pl2 === "QUEEN") {
+        } else if (scoreCard2Pl2 === "QUEEN") {
             scoreCard2Pl2 = '12'
-        } else if(scoreCard2Pl2 === "KING") {
+        } else if (scoreCard2Pl2 === "KING") {
             scoreCard2Pl2 = '13'
-        } else if(scoreCard2Pl2 === "ACE") {
+        } else if (scoreCard2Pl2 === "ACE") {
             scoreCard2Pl2 = '14'
         }
 
@@ -100,11 +111,11 @@ const Table = () => {
             let cardValue = data.value
             if (cardValue === "JACK") {
                 cardValue = '11'
-            } else if(cardValue === "QUEEN") {
+            } else if (cardValue === "QUEEN") {
                 cardValue = '12'
-            } else if(cardValue === "KING") {
+            } else if (cardValue === "KING") {
                 cardValue = '13'
-            } else if(cardValue === "ACE") {
+            } else if (cardValue === "ACE") {
                 cardValue = '14'
             }
             setCardValuePl1(prevState => prevState + Number(cardValue))
@@ -122,7 +133,6 @@ const Table = () => {
         }
         let random = (Math.random() * 2)
         random = Math.round(random)
-        console.log(random)
         const response = await axios.get(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=${random}`)
         try {
             const data = response.data.cards
@@ -130,11 +140,11 @@ const Table = () => {
                 let cardValue = item.value
                 if (cardValue === "JACK") {
                     cardValue = '11'
-                } else if(cardValue === "QUEEN") {
+                } else if (cardValue === "QUEEN") {
                     cardValue = '12'
-                } else if(cardValue === "KING") {
+                } else if (cardValue === "KING") {
                     cardValue = '13'
-                } else if(cardValue === "ACE") {
+                } else if (cardValue === "ACE") {
                     cardValue = '14'
                 }
                 setCardValuePl2(prevState => prevState + Number(cardValue))
@@ -145,52 +155,93 @@ const Table = () => {
         } catch (e) {
             console.error(e.response.status)
         }
-        console.log(enemyСards)
-        return(enemyСards)
+        return (enemyСards)
     }
 
     function setVictoryRound() {
+        let pl
         if (difference < 0 && difference2 >= 0) {
-            alert('Игрок 2 win')
+            pl = ('Игрок 2 win')
         } else if (difference2 < 0 && difference >= 0) {
-            alert('Игрок 1 win')
+            pl = ('Игрок 1 win')
         } else if ((difference < 0 && difference2 < 0) || (difference === difference2)) {
-            alert('Ничья')
+            pl = ('Ничья')
         } else if (difference2 < difference) {
-            alert('Игрок 2 win')
+            pl = ('Игрок 1 win')
         } else if (difference < difference2) {
-            alert('Игрок 1 win')
+            pl = ('Игрок 1 win')
         }
+        setHideEnemyCards(false)
+        console.log(cards2);
+        setWinner(pl)
     }
 
     useEffect(() => {
         getDeck()
+
     }, [])
 
+    
+
+    useEffect(() => {
+        enemyDrawAdditionalCard()
+    }, [cards2])
+
     return (
-        <div>
-            <div>
+        <div className='table'>
+            {cardsGivenAway ?
+                <div className='enemyCards'>
+                    {hideEnemyCards ?
+                        enemyСards.map((item, idx) => {
+                            return (
+                                <img
+                                    key={idx}
+                                    src={item.imgUrl}
+                                />
+                            )
+                        })
+                        :
+                        cards2.map((item, idx) => {
+                            return (
+                                <img
+                                    src={item.image}
+                                    alt=""
+                                />
+                            )
+                        })
+
+                    }
+                </div>
+                :
+                null
+            }
+            <p className='winner'>{winner}</p>
+            <div className='Buttons'>
+                <button onClick={drawCards}>Получить карты</button>
+                <button onClick={drawAdditionalCard}>Взять еще карту</button>
+                <button onClick={setVictoryRound}>Узнать победителя</button>
+            </div>
+            {/* <div>
                 <button onClick={drawCards}>Получить карты</button>
                 <button onClick={drawAdditionalCard}>Взять еще карту</button>
                 <button onClick={enemyDrawAdditionalCard}>Взять еще карту противник</button>
                 <button onClick={setVictoryRound}>Узнать победителя</button>
-            </div>
-            <div>{cardValuePl1} {difference}</div>
-            <div>{cardValuePl2} {difference2}</div>
+            </div> */}
+            {/* <div>{cardValuePl1} {difference}</div>
+            <div>{cardValuePl2} {difference2}</div> */}
             <div>
                 <div>
-                    {cards.map((item, idx) => <img src={item.image} alt="" />)}
-                </div>
-                {cardsGivenAway ?
-                    <div>
-                        {/* {cards2.map((item, idx) => <img key={idx} src={item.image} alt="" />)} */}
-                        {enemyСards.map((item, idx) => {return(<img key={idx} src={item.imgUrl} alt="" />) })}
+                    < div className='score'>
+                        <p>очков: {cardValuePl1}</p>
                     </div>
-                    :
-                    null
-                }
+                    <div className='cards'>
+                        {cards.map((item, idx) => {
+                            return (<img key={idx} src={item.image} />)
+                        })}
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
